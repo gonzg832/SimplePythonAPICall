322483d729955571b80cfd22493312cb1bed3c4c
Geovanni Gonzalez <91762238+gonzg832@users.noreply.github.com>
2021-10-26 22:17:44 +0000

    API call to get dog breeds

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+
+url_host = "https://dog.ceo"
+endpoint = "/api/breeds/list/all"
+query = ""
+
+url = url_host + endpoint + query
+
+response = requests.get(url)
+print(response.json())
